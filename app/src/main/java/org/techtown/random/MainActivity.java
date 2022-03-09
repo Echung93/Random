e840a33735049e668ae7f1ecc33a7015f19b4a90
Echung <kty309@gmail.com>
2022-03-09 10:43:44 +0900

    랜덤 변수 생성 코드

---
+++ app/src/main/java/org/techtown/random/MainActivity.java
@@ -0,0 +1,31 @@
+package org.techtown.random;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int a = (int)(Math.random() * 20);
+                textView.setText(String.valueOf(a));
+
+            }
+        });
+
+    }
+}
